bef351b73bd6b59966a2367dc9cc1479f43a2157
utahta <labs.ninxit@gmail.com>
2017-01-29 17:53:03 +0900

    go-bindata-assetf を使う

--- app/cmd/server/assets_fs.go
+++ app/cmd/server/assets_fs.go
@@ -1,51 +1,17 @@
 package main
 
 import (
-	"bytes"
-	"io"
-	"io/ioutil"
 	"net/http"
-	"os"
-	"strings"
 
+	"github.com/elazarl/go-bindata-assetfs"
 	"github.com/labstack/echo"
-	"github.com/pkg/errors"
 )
 
-type AssetFileSystem struct {
-	http.FileSystem
-}
-
-type AssetFile struct {
-	io.ReadSeeker
-	io.Closer
-	os.FileInfo
-}
-
 func FileServerHandler() echo.HandlerFunc {
-	return echo.WrapHandler(http.FileServer(&AssetFileSystem{}))
-}
-
-// @param name URL パスが渡ってくる。ルーティングを go-bindata で固めたパスに合わせる
-func (fs *AssetFileSystem) Open(name string) (http.File, error) {
-	path := strings.TrimLeft(name, "/")
-	data, err := Asset(path)
-	if err != nil {
-		return nil, err
+	fs := &assetfs.AssetFS{
+		Asset:     Asset,
+		AssetDir:  AssetDir,
+		AssetInfo: AssetInfo,
 	}
-	info, _ := AssetInfo(path)
-	file := &AssetFile{
-		bytes.NewReader(data),
-		ioutil.NopCloser(nil),
-		info,
-	}
-	return file, nil
-}
-
-func (f *AssetFile) Readdir(count int) ([]os.FileInfo, error) {
-	return nil, errors.New("not a directory")
-}
-
-func (f *AssetFile) Stat() (os.FileInfo, error) {
-	return f, nil
+	return echo.WrapHandler(http.FileServer(fs))
 }
